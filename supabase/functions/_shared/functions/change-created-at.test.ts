09d61f05eaa5023fb0a4bd0a5614342e960be38e
majes <agerard3@my.gcu.edu>
2025-10-14 14:43:27 -0700

    Fix

--- supabase/functions/_shared/functions/change-created-at.test.ts
+++ supabase/functions/_shared/functions/change-created-at.test.ts
@@ -2,7 +2,7 @@ import assert from "node:assert";
 import { changeCreatedAt } from "./change-created-at.ts";
 import { Session } from "../types/session.ts";
 
-Deno.test("test changeCreatedAt", async () => {
+Deno.test("test changeCreatedAt", () => {
   const session: Session = {
     user_id: "123",
     session_id: "123",
