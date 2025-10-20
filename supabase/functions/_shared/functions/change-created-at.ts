963de01aa679e39669a2b592dffbd8d8898d3bef
Dylan Lewis <disneydylan1@gmail.com>
2025-10-20 14:55:45 -0700

    feat: add user_id validation to createSession endpoint

--- supabase/functions/_shared/functions/change-created-at.ts
+++ supabase/functions/_shared/functions/change-created-at.ts
@@ -1,6 +1,6 @@
-import { Session } from "../types/session.ts";
+import { Session } from "@shared/types/session.ts";
 
 export const changeCreatedAt = (session: Session): Session => {
   session.created_at = new Date();
   return session;
-}
+};
