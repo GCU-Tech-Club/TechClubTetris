08a015af52aa2c66100639c00222df1813b763df
Blake0206 <blakealmanza02@gmail.com>
2025-10-21 16:41:36 -0700

    Fixed change-created-at

--- supabase/functions/_shared/functions/change-created-at.test.ts
+++ supabase/functions/_shared/functions/change-created-at.test.ts
@@ -1,15 +1,15 @@
-import assert from "node:assert";
+import { assertNotEquals } from "std/assert/mod.ts";
 import { changeCreatedAt } from "./change-created-at.ts";
 import { Session } from "@shared/types/session.ts";
 
-Deno.test("test changeCreatedAt", async () => {
+Deno.test("test changeCreatedAt", () => {
   const session: Session = {
     user_id: "123",
     session_id: "123",
-    created_at: new Date(),
+    created_at: new Date("2025-01-01 12:00:00 UTC"),
   };
   console.log(session);
   const sessionWithChangedCreatedAt = changeCreatedAt(session);
   console.log(sessionWithChangedCreatedAt);
-  assert(sessionWithChangedCreatedAt.created_at !== session.created_at);
+  assertNotEquals(sessionWithChangedCreatedAt.created_at, session.created_at);
 });
