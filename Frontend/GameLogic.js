6524ad1c662ab3228aea00e2eda1d300578f38df
Dylan Lewis <disneydylan1@gmail.com>
2026-02-17 17:31:01 -0700

    update shift piece left and right so pieces shift right after hitting left wall

--- Frontend/GameLogic.js
+++ Frontend/GameLogic.js
@@ -1,40 +1,40 @@
-import { TETROMINOS } from './Pieces.js';
+import { TETROMINOS } from "./Pieces.js";
 
 console.log("this is where the board goes");
 console.log(TETROMINOS);
 // Game state
 export const game = {
-    board: Array.from({ length: 20 }, () => Array(10).fill(0)),
-    activePiece: null,
-    score: 0
+  board: Array.from({ length: 20 }, () => Array(10).fill(0)),
+  activePiece: null,
+  score: 0,
 };
 
 export function getGameState() {
-    return game;
+  return game;
 }
 
 // Create a new active piece
 export function spawnPiece(tetrominoKey) {
-    const tetromino = TETROMINOS[tetrominoKey]; // 'T'
-    game.activePiece = {
-        shape: tetromino['shapes'],
-        rot: 0,
-        row: 0,
-        col: 3,
-        id: tetromino['id']
-    };
+  const tetromino = TETROMINOS[tetrominoKey]; // 'T'
+  game.activePiece = {
+    shape: tetromino["shapes"],
+    rot: 0,
+    row: 0,
+    col: 3,
+    id: tetromino["id"],
+  };
 
-    if(game.activePiece != null)
-    {
-        console.log(game.activePiece.shape[game.activePiece.rot]);
-        for (let i = 0; i < 4; i++) {
-            for (let j = 0; j < 4; j++) { // Inner loop for columns
-                if (game.activePiece.shape[game.activePiece.rot][i][j] === 1) {
-                    game.board[i + game.activePiece.row][j + game.activePiece.col] = 1;
-                }
-            }
+  if (game.activePiece != null) {
+    console.log(game.activePiece.shape[game.activePiece.rot]);
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        // Inner loop for columns
+        if (game.activePiece.shape[game.activePiece.rot][i][j] === 1) {
+          game.board[i + game.activePiece.row][j + game.activePiece.col] = 1;
         }
+      }
     }
+  }
 }
 
 // Change the active piece to the next rotation
@@ -82,12 +82,11 @@ export function rotatePiece() {
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       if (shape[game.activePiece.rot][i][j] === 1) {
-        game.board[game.activePiece.row + i][game.activePiece.col + j] = 1;;
+        game.board[game.activePiece.row + i][game.activePiece.col + j] = 1;
       }
     }
   }
 }
-
 
 // Return true if a 4x4 shape can be placed at (row,col) fully in-bounds
 // and not overlapping any solidified cells (marked as 2).
@@ -109,96 +108,95 @@ function canPlace(shape, row, col) {
 }
 
 // Shift piece down
-export function shiftPieceDown()
-{
-    if (!game.activePiece) return;
-      
-    // Erase the active piece off the board (still exists in the activePiece var)
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            if (game.activePiece.shape[game.activePiece.rot][i][j] === 1) game.board[game.activePiece.row + i][game.activePiece.col + j] = 0;
-        }
-    }
+export function shiftPieceDown() {
+  if (!game.activePiece) return;
 
-    // If we should solidify, set the active piece to null
-    if(solidifyPiece()) 
-    {
-      // Put the new piece on the board where it currently is
-      for (let i = 0; i < 4; i++) {
-          for (let j = 0; j < 4; j++) {
-              if (game.activePiece.shape[game.activePiece.rot][i][j] === 1) game.board[game.activePiece.row + i][game.activePiece.col + j] = 1;
-          }
+  // Erase the active piece off the board (still exists in the activePiece var)
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (game.activePiece.shape[game.activePiece.rot][i][j] === 1)
+        game.board[game.activePiece.row + i][game.activePiece.col + j] = 0;
+    }
+  }
+
+  // If we should solidify, set the active piece to null
+  if (solidifyPiece()) {
+    // Put the new piece on the board where it currently is
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (game.activePiece.shape[game.activePiece.rot][i][j] === 1)
+          game.board[game.activePiece.row + i][game.activePiece.col + j] = 1;
       }
-       game.activePiece = null; 
-
-       // Update the score
-       updateScore(); 
-
-       return -1;
     }
+    game.activePiece = null;
 
-    // Move the piece down if we shouldn't solidify   
-    // Increment the row to move the active piece down
-    game.activePiece.row += 1;
-        
-    // Put the new piece on the board
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            if (game.activePiece.shape[game.activePiece.rot][i][j] === 1) game.board[game.activePiece.row + i][game.activePiece.col + j] = 1;
-        }
+    // Update the score
+    updateScore();
+
+    return -1;
+  }
+
+  // Move the piece down if we shouldn't solidify
+  // Increment the row to move the active piece down
+  game.activePiece.row += 1;
+
+  // Put the new piece on the board
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (game.activePiece.shape[game.activePiece.rot][i][j] === 1)
+        game.board[game.activePiece.row + i][game.activePiece.col + j] = 1;
     }
-    return 1;
+  }
+  return 1;
 }
 
 // Checks for complete rows and adds to the score
 // returns the score to add to the main score
 export function updateScore() {
+  let completeRows = 0;
+  let currentScore = 0;
 
-    let completeRows = 0;
-    let currentScore = 0;
-
-    // iterate through 2d array board and check for complete rows //
-    for (let i = 0; i < 20; i++) {
-        let isRowComplete = true;
-        for (let j = 0; j < 10; j++) {
-            // If the board is 0 at any point, row is incomplete.
-            if (game.board[i][j] === 0) {
-                isRowComplete = false;
-                break;
-            } 
-        }
-        // after iterating through a row check to see if isRowComplete is true
-        if (isRowComplete) {
-            completeRows++; 
-
-            // Remove this row and add a new empty row at the top
-            removeCompleteRow(i); 
-            i--;
-        }
+  // iterate through 2d array board and check for complete rows //
+  for (let i = 0; i < 20; i++) {
+    let isRowComplete = true;
+    for (let j = 0; j < 10; j++) {
+      // If the board is 0 at any point, row is incomplete.
+      if (game.board[i][j] === 0) {
+        isRowComplete = false;
+        break;
+      }
     }
-    // add to score based on the number of complete rows
-    // 1 row = 100pts
-    // 2 rows = 200pts
-    // 3 rows = 300pts
-    // 4 rows = 400pts
-    currentScore = completeRows * 100;
-    game.score += currentScore;
-    updateScoreUI();
+    // after iterating through a row check to see if isRowComplete is true
+    if (isRowComplete) {
+      completeRows++;
 
-    //return currentScore; 
+      // Remove this row and add a new empty row at the top
+      removeCompleteRow(i);
+      i--;
+    }
+  }
+  // add to score based on the number of complete rows
+  // 1 row = 100pts
+  // 2 rows = 200pts
+  // 3 rows = 300pts
+  // 4 rows = 400pts
+  currentScore = completeRows * 100;
+  game.score += currentScore;
+  updateScoreUI();
+
+  //return currentScore;
 }
 
-// clear complete lines 
+// clear complete lines
 // add new row to the top
 export function removeCompleteRow(rowIndex) {
-    // set all of the values of the complete row back to 0
-    game.board.splice(rowIndex, 1);
-    game.board.unshift(Array(10).fill(0));
-        
+  // set all of the values of the complete row back to 0
+  game.board.splice(rowIndex, 1);
+  game.board.unshift(Array(10).fill(0));
 }
 function updateScoreUI() {
-    const scoreElement = document.getElementById('score-box');
-    scoreElement.innerText = `Score: ${game.score}`;
+  const scoreElement = document.getElementById("score-box");
+  scoreElement.innerText = `Score: ${game.score}`;
 }
 
 // Checks if we should solidify piece, returns boolean
@@ -222,85 +220,67 @@ export function solidifyPiece() {
 }
 
 export function printBoard() {
-    for (let i = 0; i < 20; i++) {
-        console.log(game.board[i].toString());
-    }
-    console.log(game.board[2].toString());
+  for (let i = 0; i < 20; i++) {
+    console.log(game.board[i].toString());
+  }
+  console.log(game.board[2].toString());
 }
 
 export function shiftPieceLeft() {
-    let isAgainstEdge = false;
+  if (game.activePiece.col <= 0) return;
 
-    if ((game.activePiece.col === 0) || (game.activePiece.col === 9)) {
-            isAgainstEdge = true;
-            return;
-        }
-
-    // Erase the current piece off the board 
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            if (game.activePiece.shape[game.activePiece.rot][i][j] === 1)
-                game.board[i+ game.activePiece.row][j + game.activePiece.col] = 0;
-        }
+  // Erase the current piece off the board
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (game.activePiece.shape[game.activePiece.rot][i][j] === 1) {
+        game.board[i + game.activePiece.row][j + game.activePiece.col] = 0;
+      }
     }
+  }
 
-    if(!isAgainstEdge)
-    {
-        game.activePiece.col -= 1;
-        for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            if (game.activePiece.shape[game.activePiece.rot][i][j] === 1) {
-                game.board[i + game.activePiece.row][j + game.activePiece.col] = 1;
-            }
-        }
-    }
-    }
-    // Put the new piece on the board
+  game.activePiece.col -= 1; // move left
 
-    
-    // Checks if the piece is to the edge it is going to stop shifting
-    
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (game.activePiece.shape[game.activePiece.rot][i][j] === 1) {
+        game.board[i + game.activePiece.row][j + game.activePiece.col] = 1;
+      }
+    }
+  }
 }
-
-
-
 
 export function shiftPieceRight() {
-    let pieceLength = 0;
-    const lenghts = game.activePiece.shape[game.activePiece.rot].map(row => row.filter(num => num === 1).length);
-    pieceLength =Math.max(...lenghts);
+  //   let pieceLength = 0;
+  const lenghts = game.activePiece.shape[game.activePiece.rot].map(
+    (row) => row.filter((num) => num === 1).length,
+  );
+  let pieceLength = Math.max(...lenghts);
 
-    let isAgainstEdge = false;
+  // Check only right edge
+  if (game.activePiece.col + pieceLength - 1 >= 9) return;
 
-    if ((game.activePiece.col === 0) || (game.activePiece.col + pieceLength - 1 === 9)) {
-            isAgainstEdge = true;
-            return;
-        }
-
-    // Erase the current piece off the board 
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            if (game.activePiece.shape[game.activePiece.rot][i][j] === 1)
-                game.board[i+ game.activePiece.row][j + game.activePiece.col] = 0;
-        }
+  // Erase the current piece off the board
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (game.activePiece.shape[game.activePiece.rot][i][j] === 1) {
+        game.board[i + game.activePiece.row][j + game.activePiece.col] = 0;
+      }
     }
+  }
 
-    if(!isAgainstEdge)
-    {
-        game.activePiece.col += 1;
-        for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            if (game.activePiece.shape[game.activePiece.rot][i][j] === 1) {
-                game.board[i + game.activePiece.row][j + game.activePiece.col] = 1;
-            }
-        }
+  game.activePiece.col += 1; // move right
+
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (game.activePiece.shape[game.activePiece.rot][i][j] === 1) {
+        game.board[i + game.activePiece.row][j + game.activePiece.col] = 1;
+      }
     }
-}
+  }
 }
 
-
-  // THESE ARE FOR TESTING DELETE THEM WHEN DONE //
-  window.game = game;
-  window.removeCompleteRow = removeCompleteRow;
-  window.updateScore = updateScore;
-  // THESE ARE FOR TESTING DELETE THEM DONE //
+// THESE ARE FOR TESTING DELETE THEM WHEN DONE //
+window.game = game;
+window.removeCompleteRow = removeCompleteRow;
+window.updateScore = updateScore;
+// THESE ARE FOR TESTING DELETE THEM DONE //
